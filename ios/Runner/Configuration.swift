64856ca26aafce5d51f5822a6c161136e9410495
rkdev16 <139223425+rkdev16@users.noreply.github.com>
2025-04-29 12:25:08 +0530

    Delete ios directory

--- ios/Runner/Configuration.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-public enum Configuration {
-    static let lensGroupId = "dfc17eb3-faa2-40ff-8f3c-751a3ae1218f"
-    static let applicationId = "7bcc8fb2-3187-4a08-b031-825c240ce345"
-    static let apiToken = "eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNzI5MjQ5NDMyLCJzdWIiOiI3YmNjOGZiMi0zMTg3LTRhMDgtYjAzMS04MjVjMjQwY2UzNDV-UFJPRFVDVElPTn43YmZhZWYxMy0zZWJlLTQzYmEtOTNmMi0xYWU3ZDdhNzYwMzkifQ.AGnALr-jdko6Q5HsGQBiHnxhWrVkZjFIKhrQjseR1u8"
-}
-
